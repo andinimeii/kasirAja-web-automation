76e5580dd99dcfb5182e163ee6e7755d8459d864
Andini Mei <andinimeiliawati@gmail.com>
2024-04-02 15:44:53 +0700

    test suite for customers

---
+++ cypress/e2e/pelanggan.cy.js
@@ -0,0 +1,59 @@
+const HomePage = require('../support/pages/home-page')
+const LoginPage = require('../support/pages/login-page') 
+const PelangganPage = require('../support/pages/pelanggan-page')
+
+describe('User should be able to manage customers', () => {
+    it('Add customers - Valid', () => {
+
+        // 1. Visit Url
+        HomePage.goHomepage()
+
+        // 2. Login
+        LoginPage.fillEmail()
+        LoginPage.fillPassword()
+        LoginPage.clickLoginButton()
+
+        // 3. Add Customers
+        PelangganPage.clickPelangganMenu()
+        PelangganPage.clickPelangganAdd()
+        PelangganPage.fillPelangganName()
+        PelangganPage.fillPelangganPhone()
+        PelangganPage.fillPelangganAddress()
+        PelangganPage.fillPelangganKet()
+        PelangganPage.clickPelangganBtn()
+
+    })
+
+    it('Edit customers - Valid', () => {
+        // 1. Visit Url
+        HomePage.goHomepage()
+
+        // 2. Login
+        LoginPage.fillEmail()
+        LoginPage.fillPassword()
+        LoginPage.clickLoginButton()
+
+        // 3. Edit Customers Data
+        PelangganPage.clickPelangganMenu()
+        PelangganPage.clickMenuList()
+        PelangganPage.clickPelangganEdit()
+        PelangganPage.fillEditPelangganName()
+        PelangganPage.clickPelangganBtn()
+        PelangganPage.goPelangganPage()
+    })
+
+    it('Delete customers - Valid', () => {
+        // 1. Visit Url
+        HomePage.goHomepage()
+
+        // 2. Login
+        LoginPage.fillEmail()
+        LoginPage.fillPassword()
+        LoginPage.clickLoginButton()
+
+        // 3. Delete Customers
+        PelangganPage.clickPelangganMenu()
+        PelangganPage.clickMenuList()
+        PelangganPage.clickPelangganDelete()
+    })
+})
