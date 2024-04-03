023010d1e6d30a245bb1483f8abfd4143ea740f3
Andini Mei <andinimeiliawati@gmail.com>
2024-04-03 20:06:43 +0700

    update locators

--- cypress/support/locators/penjualan-page-locators.js
+++ cypress/support/locators/penjualan-page-locators.js
@@ -5,6 +5,7 @@ module.exports = {
         product_menu: '//button[contains(text(), "produk")]',
         product_add: '//*[@id="chakra-modal--body-31"]/table/tbody/tr',
         total: '//*[@id="root"]/div/div/div[2]/div[2]/div/div[2]/div[2]/table/tbody/tr/td[4]/input',
+        removeCustomer: '(//div[@class="chakra-input__right-addon css-7nrq"])[1]',
         pay_button: '//button[contains(text(), "Bayar")]',
         confirm_button: '//button[contains(text(), "Ya")]',
         close_button: '//button[contains(text(), "tutup")]',
